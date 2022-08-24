4fa9f8546aedca6aad31e4ca325dda276af1f34b
Maksim <lihoi.maksim@gmail.com>
2022-08-24 14:34:43 +0300

    Installed redux, refactoring for components with useNavigation is needed

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import BottomTabNavigator from './src/navigation/BottomTabNavigator';
+import {Provider} from 'react-redux';
+import store from 'src/store';
+import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
+
+const persistor = persistStore(store);
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <NavigationContainer>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <BottomTabNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+          <BottomTabNavigator />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
