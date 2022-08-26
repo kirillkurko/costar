e98eac487dd0b9a213e682043a9f1ecddc9949d1
Maksim <lihoi.maksim@gmail.com>
2022-08-26 15:05:58 +0300

    Revert "Installed redux, refactoring for components with useNavigation is needed"
    This reverts commit 4fa9f854

--- App.js
+++ App.js
@@ -2,25 +2,15 @@ import React from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import BottomTabNavigator from './src/navigation/BottomTabNavigator';
-import {Provider} from 'react-redux';
-import store from 'src/store';
-import {persistStore} from 'redux-persist';
-import {PersistGate} from 'redux-persist/integration/react';
-
-const persistor = persistStore(store);
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <BottomTabNavigator />
-        </NavigationContainer>
-      </PersistGate>
-    </Provider>
+    <NavigationContainer>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <BottomTabNavigator />
+    </NavigationContainer>
   );
 };
 
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,11 @@
-import {persistReducer} from 'redux-persist';
+import { persistReducer } from 'redux-persist';
 
 import storage from 'redux-persist/lib/storage';
-import {reducer} from './reducer';
+import { reducer } from './reducer';
 
 const persistConfig = {
-  key: 'root',
-  storage,
+    key: 'root',
+    storage,
 };
 
 export default persistReducer(persistConfig, reducer);
