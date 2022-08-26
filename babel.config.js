9a81c5be624ca29812dbbac3ff8a192e78946b3c
Maksim <lihoi.maksim@gmail.com>
2022-08-26 15:13:04 +0300

    Revert "Typescript config added"
    This reverts commit 5c052f88cc7be894785c476c5dd8bb64dc3bdea2.

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: [
-          '.js',
-          '.jsx',
-          '.ts',
-          '.tsx',
-          '.android.js',
-          '.android.tsx',
-          '.ios.js',
-          '.ios.tsx',
-        ],
-        alias: {
-          '@shared': './src/shared',
-          '@components': './src/components',
-          '@utils': './src/utils',
-          '@assets': './src/assets',
-          '@screens': './src/screens',
-        },
-      },
-    ],
-  ],
 };
