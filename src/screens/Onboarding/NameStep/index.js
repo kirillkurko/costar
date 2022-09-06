b20923248984a66be7a022df819e6e194ce10970
Maksim <lihoi.maksim@gmail.com>
2022-09-06 15:22:30 +0300

    Refactored NameStep screen from class component to functional

--- src/screens/Onboarding/NameStep/index.js
+++ src/screens/Onboarding/NameStep/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Animated,
   Dimensions,
@@ -12,7 +12,7 @@ import {
   View,
 } from 'react-native';
 import { connect } from 'react-redux';
-import { SafeAreaConsumer } from 'react-native-safe-area-context';
+import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
 import LinearGradient from 'react-native-linear-gradient';
 import type { Dispatch as ReduxDispatch } from 'redux';
 
@@ -26,6 +26,7 @@ import { setUserName } from 'src/store/actions';
 import styles from './styles';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { OnboardingStackNavigatorRouts } from '../../../variables/navigationRouts';
+import { useNavigation } from '@react-navigation/native';
 
 const { State: TextInputState } = TextInput;
 
@@ -40,40 +41,38 @@ type State = {
   placeholder: string,
 };
 
-class NameStep extends PureComponent<Props, State> {
-  constructor(props) {
-    super(props);
-    this.textInput = React.createRef();
-    this.state = {
-      value: '',
-      shift: new Animated.Value(0),
-      isAnimate: true,
-      placeholder: resources.t('ONBOARDING.PLACEHOLDER'),
-    };
-  }
+const NameStep = (props) => {
+  const textInput = React.createRef();
+  const navigation = useNavigation();
 
-  componentDidMount() {
-    this.keyboardDidShowSub = Keyboard.addListener(
+  const [state, setState] = useState({
+    value: '',
+    shift: new Animated.Value(0),
+    isAnimate: true,
+    placeholder: resources.t('ONBOARDING.PLACEHOLDER'),
+  });
+
+  useEffect(() => {
+    const keyboardDidShowSub = Keyboard.addListener(
       'keyboardDidShow',
-      this.handleKeyboardDidShow,
+      handleKeyboardDidShow,
     );
-    this.keyboardDidHideSub = Keyboard.addListener(
+    const keyboardDidHideSub = Keyboard.addListener(
       'keyboardDidHide',
-      this.handleKeyboardDidHide,
+      handleKeyboardDidHide,
     );
-  }
+    return () => {
+      keyboardDidShowSub.remove();
+      keyboardDidHideSub.remove();
+    };
+  }, []);
 
-  componentWillUnmount() {
-    this.keyboardDidShowSub.remove();
-    this.keyboardDidHideSub.remove();
-  }
-
-  onChangeText = (value) => {
-    this.setState({ value });
+  const onChangeText = (value) => {
+    setState({ ...state, value });
   };
 
-  handleKeyboardDidShow = (event) => {
-    const { shift } = this.state;
+  const handleKeyboardDidShow = (event) => {
+    const { shift } = state;
     const { height: windowHeight } = Dimensions.get('window');
     const keyboardHeight = event.endCoordinates.height;
     const currentlyFocusedField = TextInputState.currentlyFocusedField();
@@ -93,8 +92,8 @@ class NameStep extends PureComponent<Props, State> {
     );
   };
 
-  handleKeyboardDidHide = () => {
-    const { shift } = this.state;
+  const handleKeyboardDidHide = () => {
+    const { shift } = state;
     Animated.timing(shift, {
       toValue: 0,
       duration: 500,
@@ -102,195 +101,192 @@ class NameStep extends PureComponent<Props, State> {
     }).start();
   };
 
-  setIsAnimate = () => {
-    this.setState({ isAnimate: false });
+  const setIsAnimate = () => {
+    setState({ ...state, isAnimate: false });
   };
 
-  onFocus = () => {
-    this.setState({ placeholder: '' });
+  const onFocus = () => {
+    setState({ ...state, placeholder: '' });
   };
 
-  onBlur = () => {
-    this.setState({ placeholder: resources.t('ONBOARDING.PLACEHOLDER') });
+  const onBlur = () => {
+    setState({ ...state, placeholder: resources.t('ONBOARDING.PLACEHOLDER') });
   };
 
-  onButtonPress = async () => {
-    const { dispatch } = this.props;
-    const { value } = this.state;
+  const onButtonPress = async () => {
+    const { dispatch } = props;
+    const { value } = state;
     const userName = value.trim() || 'Name';
-    this.textInput.current.blur();
+    textInput.current.blur();
     await AsyncStorage.setItem('name', userName);
     dispatch(setUserName(userName));
-    const { navigation } = this.context;
     navigation.navigate(OnboardingStackNavigatorRouts.BirthdayStep);
   };
 
-  render() {
-    const { value, shift, isAnimate, placeholder } = this.state;
-    return (
-      <SafeAreaConsumer>
-        {(insets) => (
-          <ImageBackground
-            source={img.onboarding.firstBg}
-            style={[
-              styles.container,
-              {
-                paddingTop: insets.top,
-                paddingBottom: insets.bottom,
-              },
-            ]}>
-            <OnboardingHeader number={1} />
-            <Animated.View
-              style={[styles.wrapper, { transform: [{ translateY: shift }] }]}>
-              <View>
-                <Text style={styles.titleText}>
-                  {resources.t('ONBOARDING.NAME.TITLE')}
-                </Text>
-                <Text style={styles.descriptionText}>
-                  {resources.t('ONBOARDING.NAME.DESCRIPTION')}
-                </Text>
-                <View style={styles.imageWrapper}>
-                  <Animated.Image
-                    style={{
-                      ...styles.circle,
-                      opacity: isAnimate
-                        ? Animate({
-                            duration: 600,
-                            delay: 100,
-                            useNativeDriver: true,
-                          })
-                        : 1,
-                    }}
-                    source={img.onboarding.circleTopShadow}
-                    showWebviewLoader={false}
-                    resizeMode='contain'
-                  />
-                  <Animated.Image
-                    style={{
-                      ...styles.man,
-                      opacity: isAnimate
-                        ? Animate({
+  const { value, shift, isAnimate, placeholder } = state;
+  return (
+    <SafeAreaInsetsContext.Consumer>
+      {(insets) => (
+        <ImageBackground
+          source={img.onboarding.firstBg}
+          style={[
+            styles.container,
+            {
+              paddingTop: insets.top,
+              paddingBottom: insets.bottom,
+            },
+          ]}>
+          <OnboardingHeader number={1} />
+          <Animated.View
+            style={[styles.wrapper, { transform: [{ translateY: shift }] }]}>
+            <View>
+              <Text style={styles.titleText}>
+                {resources.t('ONBOARDING.NAME.TITLE')}
+              </Text>
+              <Text style={styles.descriptionText}>
+                {resources.t('ONBOARDING.NAME.DESCRIPTION')}
+              </Text>
+              <View style={styles.imageWrapper}>
+                <Animated.Image
+                  style={{
+                    ...styles.circle,
+                    opacity: isAnimate
+                      ? Animate({
+                          duration: 600,
+                          delay: 100,
+                          useNativeDriver: true,
+                        })
+                      : 1,
+                  }}
+                  source={img.onboarding.circleTopShadow}
+                  showWebviewLoader={false}
+                  resizeMode='contain'
+                />
+                <Animated.Image
+                  style={{
+                    ...styles.man,
+                    opacity: isAnimate
+                      ? Animate({
+                          duration: 700,
+                          delay: 350,
+                          useNativeDriver: true,
+                        })
+                      : 1,
+                  }}
+                  source={img.onboarding.firstScreenMan}
+                  showWebviewLoader={false}
+                  resizeMode='contain'
+                />
+                <Animated.Image
+                  style={{
+                    ...styles.seven,
+                    opacity: isAnimate
+                      ? Animate({
+                          duration: 700,
+                          delay: 600,
+                          useNativeDriver: true,
+                        })
+                      : 1,
+                  }}
+                  source={img.onboarding.seven}
+                  showWebviewLoader={false}
+                  resizeMode='contain'
+                />
+                <Animated.Image
+                  style={{
+                    ...styles.energy,
+                    opacity: isAnimate
+                      ? Animate({
+                          duration: 700,
+                          delay: 400,
+                          useNativeDriver: true,
+                        })
+                      : 1,
+                  }}
+                  source={img.onboarding.firstScreenEnergy}
+                  showWebviewLoader={false}
+                  resizeMode='contain'
+                />
+                <Animated.Image
+                  style={{
+                    ...styles.five,
+                    opacity: isAnimate
+                      ? Animate({
+                          duration: 700,
+                          delay: 700,
+                          useNativeDriver: true,
+                        })
+                      : 1,
+                  }}
+                  source={img.onboarding.five}
+                  showWebviewLoader={false}
+                  resizeMode='contain'
+                />
+                <Animated.Image
+                  style={{
+                    ...styles.head,
+                    opacity: isAnimate
+                      ? Animate(
+                          {
                             duration: 700,
-                            delay: 350,
+                            delay: 800,
                             useNativeDriver: true,
-                          })
-                        : 1,
-                    }}
-                    source={img.onboarding.firstScreenMan}
-                    showWebviewLoader={false}
-                    resizeMode='contain'
-                  />
-                  <Animated.Image
-                    style={{
-                      ...styles.seven,
-                      opacity: isAnimate
-                        ? Animate({
-                            duration: 700,
-                            delay: 600,
-                            useNativeDriver: true,
-                          })
-                        : 1,
-                    }}
-                    source={img.onboarding.seven}
-                    showWebviewLoader={false}
-                    resizeMode='contain'
-                  />
-                  <Animated.Image
-                    style={{
-                      ...styles.energy,
-                      opacity: isAnimate
-                        ? Animate({
-                            duration: 700,
-                            delay: 400,
-                            useNativeDriver: true,
-                          })
-                        : 1,
-                    }}
-                    source={img.onboarding.firstScreenEnergy}
-                    showWebviewLoader={false}
-                    resizeMode='contain'
-                  />
-                  <Animated.Image
-                    style={{
-                      ...styles.five,
-                      opacity: isAnimate
-                        ? Animate({
-                            duration: 700,
-                            delay: 700,
-                            useNativeDriver: true,
-                          })
-                        : 1,
-                    }}
-                    source={img.onboarding.five}
-                    showWebviewLoader={false}
-                    resizeMode='contain'
-                  />
-                  <Animated.Image
-                    style={{
-                      ...styles.head,
-                      opacity: isAnimate
-                        ? Animate(
-                            {
-                              duration: 700,
-                              delay: 800,
-                              useNativeDriver: true,
-                            },
-                            this.setIsAnimate,
-                          )
-                        : 1,
-                    }}
-                    source={img.onboarding.firstScreenHead}
-                    showWebviewLoader={false}
-                    resizeMode='contain'
-                  />
-                  <Animated.Image
-                    style={{
-                      ...styles.shadow,
-                      opacity: isAnimate
-                        ? Animate({
-                            duration: 700,
-                            delay: 300,
-                            useNativeDriver: true,
-                          })
-                        : 1,
-                    }}
-                    source={img.onboarding.shadow}
-                    showWebviewLoader={false}
-                    resizeMode='contain'
-                  />
-                </View>
-              </View>
-              <View>
-                <LinearGradient
-                  colors={colors.violetGradient}
-                  style={styles.inputWrapper}
-                  start={{ x: 0, y: 0 }}
-                  end={{ x: 1, y: 0 }}>
-                  <TextInput
-                    ref={this.textInput}
-                    style={styles.inputStyle}
-                    onChangeText={this.onChangeText}
-                    value={value}
-                    placeholder={placeholder}
-                    selectionColor={colors.white}
-                    placeholderTextColor={colors.gray}
-                    onFocus={this.onFocus}
-                    onBlur={this.onBlur}
-                  />
-                </LinearGradient>
-                <BigButton
-                  style={styles.buttonWrapper}
-                  onPress={this.onButtonPress}
-                  buttonText={resources.t('ONBOARDING.BUTTONS.NEXT')}
-                  withArrowIcon
+                          },
+                          setIsAnimate,
+                        )
+                      : 1,
+                  }}
+                  source={img.onboarding.firstScreenHead}
+                  showWebviewLoader={false}
+                  resizeMode='contain'
+                />
+                <Animated.Image
+                  style={{
+                    ...styles.shadow,
+                    opacity: isAnimate
+                      ? Animate({
+                          duration: 700,
+                          delay: 300,
+                          useNativeDriver: true,
+                        })
+                      : 1,
+                  }}
+                  source={img.onboarding.shadow}
+                  showWebviewLoader={false}
+                  resizeMode='contain'
                 />
               </View>
-            </Animated.View>
-          </ImageBackground>
-        )}
-      </SafeAreaConsumer>
-    );
-  }
-}
+            </View>
+            <View>
+              <LinearGradient
+                colors={colors.violetGradient}
+                style={styles.inputWrapper}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}>
+                <TextInput
+                  ref={textInput}
+                  style={styles.inputStyle}
+                  onChangeText={onChangeText}
+                  value={value}
+                  placeholder={placeholder}
+                  selectionColor={colors.white}
+                  placeholderTextColor={colors.gray}
+                  onFocus={onFocus}
+                  onBlur={onBlur}
+                />
+              </LinearGradient>
+              <BigButton
+                style={styles.buttonWrapper}
+                onPress={onButtonPress}
+                buttonText={resources.t('ONBOARDING.BUTTONS.NEXT')}
+                withArrowIcon
+              />
+            </View>
+          </Animated.View>
+        </ImageBackground>
+      )}
+    </SafeAreaInsetsContext.Consumer>
+  );
+};
 
 export default connect()(NameStep);
