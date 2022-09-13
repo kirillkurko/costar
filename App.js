e0077f5d00e8d402c7353bd9fd18a6cb07d16585
Kirill Kurko <kirillkurko2609@gmail.com>
2022-09-13 14:29:26 +0300

    Amplitude configured

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
@@ -7,11 +7,17 @@ import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import RootStackNavigator from './src/navigation/RootStackNavigator';
 import purchasesInteractions from './src/shared/purchases/interactions';
+import { AMPLITUDE_API_KEY } from './src/utils/constants/const';
+import { init } from '@amplitude/analytics-react-native';
 
 const persistor = persistStore(store);
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    init(AMPLITUDE_API_KEY);
+  }, []);
 
   purchasesInteractions.setup();
 
